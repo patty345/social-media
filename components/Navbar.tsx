5a9793a316967cd49b24d5d07a17e2b4faeb78f6
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-07-21 09:35:58 -0500

    adding navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { GoogleLogin, GoogleLogout } from 'react-google-login';
+import { AiOutlineLogout } from 'react-icons/ai';
+import { BiSearch } from 'react-icons/bi';
+import { IoMdAdd } from 'react-icons/io'
+
+import Logo from '../utils/tiktik-logo.png'
+
+const Navbar = () => {
+  return (
+    <div>
+        <Link href="/">
+        <div>
+            <Image className='cursor-pointer' src={Logo} alt='logo' layout='responsive'/>
+        </div>
+        </Link>
+    </div>
+  )
+}
+
+export default Navbar
