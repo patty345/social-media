6bcb2348217ebf47d396496f7276073f4bc41ca6
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-07-20 19:33:45 -0500

    new changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { useState, useEffect } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import '../styles/globals.css'
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const [isSSR, setIsSSR] = useState(true);
+
+  useEffect(() => {
+      setIsSSR(false);
+  }, []);
+
+  if(isSSR) return null;
+
+
+  return (
+    <div>
+      Navbar 
+      <div className='flex gap-6 md:gap-20'>
+        <div className='j-[92vh] overflow-hidden x1:hover:overflow-auto'>
+          Sidebar 
+        </div>
+        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] vidoes flex-1'>
+          <Component {...pageProps} />
+        </div>
+      </div>
+      
+    </div>
+  ) 
 }
 
 export default MyApp
