dbe2bfd4bc9b92167618b3a704fa7597c80930dc
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-07-27 12:48:51 -0500

    getting google auth

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { GoogleLogin, GoogleLogout } from 'react-google-login';
-import { AiOutlineLogout } from 'react-icons/ai';
-import { BiSearch } from 'react-icons/bi';
-import { IoMdAdd } from 'react-icons/io'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import {GoogleLogin, googleLogout } from '@react-oauth/google';
+import { AiOutlineLogout } from "react-icons/ai";
+import { BiSearch } from "react-icons/bi";
+import { IoMdAdd } from "react-icons/io";
 
-import Logo from '../utils/tiktik-logo.png'
+import Logo from "../utils/tiktik-logo.png";
+import { createOrGetUser } from "../utils";
 
 const Navbar = () => {
-  return (
-    <div className='w-full flex justify-between items-center border-b-2 border-gray 200 py-2 px-4'>
-        <Link href="/">
-        <div className='w-[100px] md:w-[130px]'>
-            <Image className='cursor-pointer' src={Logo} alt='logo' layout='responsive'/>
-        </div>
-        </Link>
-    </div>
-  )
-}
+  const user = false;
 
-export default Navbar
+  return (
+    <div className="w-full flex justify-between items-center border-b-2 border-gray 200 py-2 px-4">
+      <Link href="/">
+        <div className="w-[100px] md:w-[130px]">
+          <Image
+            className="cursor-pointer"
+            src={Logo}
+            alt="logo"
+            layout="responsive"
+          />
+        </div>
+      </Link>
+      <div>SEARCH</div>
+      <div>
+        {user ? (
+          <div>Logged In</div>
+        ): (
+          <GoogleLogin 
+            onSuccess={(response) => createOrGetUser(response) }
+            onError={() => console.log('Error')}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,34 @@
-import type { AppProps } from 'next/app';
-import { useState, useEffect } from 'react';
+import type { AppProps } from "next/app";
+import { useState, useEffect } from "react";
 
-import Navbar from '../components/Navbar';
-import Sidebar from '../components/Sidebar';
-import '../styles/globals.css'
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import "../styles/globals.css";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
 
   useEffect(() => {
-      setIsSSR(false);
+    setIsSSR(false);
   }, []);
 
-  if(isSSR) return null;
-
+  if (isSSR) return null;
 
   return (
-    <div>
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
       <Navbar />
-      <div className='flex gap-6 md:gap-20'>
-        <div className='j-[92vh] overflow-hidden x1:hover:overflow-auto'>
+      <div className="flex gap-6 md:gap-20">
+        <div className="j-[92vh] overflow-hidden x1:hover:overflow-auto">
           <Sidebar />
         </div>
-        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] vidoes flex-1'>
+        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] vidoes flex-1">
           <Component {...pageProps} />
         </div>
       </div>
-      
-    </div>
-  ) 
-}
+    </GoogleOAuthProvider>
+  );
+};
 
-export default MyApp
+export default MyApp;
