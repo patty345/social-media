12ef6d3e5c550fc282b8db26c768693fbaea2546
patriciaderieux <6089242+patty345@users.noreply.github.com>
2022-08-30 19:17:33 -0500

    finish with the likes button

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -6,15 +6,25 @@ import useAuthStore from '../store/authStore';
 interface IProps {
     handleLike: () => void;
     handleDislike: () => void;
+    likes: any[];
 }   
 
-const LikeButton = ({ handleLike, handleDislike }: IProps) => {
-    const [alreadyLiked, setalreadyLiked] = useState(true);
-    const { userProfile } = useAuthStore();
+const LikeButton = ({ likes, handleLike, handleDislike }: IProps) => {
+    const [alreadyLiked, setalreadyLiked] = useState(false);
+    const { userProfile }: any = useAuthStore();
+    const filterLikes = likes?.filter((item) => item._ref === userProfile?._id);
+
+    useEffect(() => {
+        if(filterLikes?.length > 0 ) {
+            setalreadyLiked(true);
+        } else {
+            setalreadyLiked(false);
+        }
+    }, [filterLikes, likes]);
 
 
   return (
-    <div className='gap-6'>
+    <div className='flex gap-6'>
         <div className='mt-4 flex flex-col justify-center items-center cursor-pointer'>
             {alreadyLiked ? (
                 <div className='bg-primary rounded-full p-2 md:p-4 text-[#f51997]' onClick={handleDislike}>
@@ -25,7 +35,7 @@ const LikeButton = ({ handleLike, handleDislike }: IProps) => {
                     <MdFavorite className='text-lg md:text-2xl' />
                 </div>
             )}
-            <p className='text-md font-semibold'>likes?.length || 0 </p>
+            <p className='text-md font-semibold'>{likes?.length || 0 }</p>
         </div>
     </div>
   )
